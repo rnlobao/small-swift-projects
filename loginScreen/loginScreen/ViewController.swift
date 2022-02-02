bffe9042fb2b48000cd1dca80ee879f385b9e02b
Robson Novato <66230142+rnlobao@users.noreply.github.com>
2022-02-02 02:01:38 -0300

    s

--- loginScreen/loginScreen/ViewController.swift
+++ loginScreen/loginScreen/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  loginScreen
 //
-//  Created by Robson Novato Lobao on 29/01/22.
+//  Created by rncodes on 29/01/22.
 //
 
 import UIKit
@@ -14,6 +14,39 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBOutlet weak var firstTextField: UITextField! {
+        didSet {
+            firstTextField.layer.cornerRadius = 8.2
+            firstTextField.leftViewMode = UITextField.ViewMode.always
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 16.37, height: 16.37))
+            let image = UIImage(named: "mess")
+            imageView.image = image
+            firstTextField.leftView = imageView
+        }
+    }
+    
+    
+    @IBOutlet weak var secondTextField: UITextField! {
+        didSet {
+            secondTextField.layer.cornerRadius = 8.2
+            secondTextField.leftViewMode = UITextField.ViewMode.always
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 16.37, height: 16.37))
+            let image = UIImage(named: "p")
+            imageView.image = image
+            secondTextField.leftView = imageView
+        }
+    }
+}
 
+extension UITextField{
+   @IBInspectable var placeHolderColor: UIColor? {
+        get {
+            return self.placeHolderColor
+        }
+        set {
+            self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSAttributedString.Key.foregroundColor: newValue!])
+        }
+    }
 }
 
